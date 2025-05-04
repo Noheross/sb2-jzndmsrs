3eb6ac08c106e4f48472760f9f9d4095e980ba72
Noheross <34516898+Noheross@users.noreply.github.com>
2025-05-04 14:07:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    lib: {
+      entry: 'src/main.tsx',
+      formats: ['esm'],
+    },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
